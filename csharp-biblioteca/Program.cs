fdd21c9a579bc84bff576df7f1ef2564de97d4af
MaxPredathor <max.gilli98@gmail.com>
2024-04-22 18:01:33 +0200

    Eesercizio completato

--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Biblioteca BibliotecaDiBologna = new Biblioteca();
+            BibliotecaDiBologna.AggiungiUtente(new Utente("Max", "Gilli", "max.gilli98@gmail.com", "pwtest", 3333333));
+            BibliotecaDiBologna.AggiungiUtente(new Utente("Luca", "Rossi", "luca.rossi@gmail.com", "pwtest", 3338543));
+            BibliotecaDiBologna.AggiungiUtente(new Utente("Mario", "Neri", "mario.neri@gmail.com", "pwtest", 3342353));
+            BibliotecaDiBologna.AggiungiUtente(new Utente("Giorgio", "Bianchi", "giorgio.bianchi@gmail.com", "pwtest", 3334653));
+            BibliotecaDiBologna.AggiungiUtente(new Utente("Matteo", "Rosi", "matteo.rosi@gmail.com", "pwtest", 3332563));
+            Console.WriteLine("Utenti:");
+            BibliotecaDiBologna.StampaLista(BibliotecaDiBologna.Utenti);
+
+            BibliotecaDiBologna.AggiungiDocumento(new Dvd(0, "Jurassic Park", 1993, new List<string> { "avventura", "azione" }, "2F", new Autore("Spielberg", "Steven"), 120));
+            BibliotecaDiBologna.AggiungiDocumento(new Dvd(1, "Il Padrino", 1972, new List<string> { "Crime", "Drama" }, "5B", new Autore("Francis", "Ford Coppola"), 120));
+            BibliotecaDiBologna.AggiungiDocumento(new Dvd(2, "Forrest Gump", 1994, new List<string> { "Drama", "Romance" }, "9K", new Autore("Robert", "Zemeckis"), 120));
+            BibliotecaDiBologna.AggiungiDocumento(new Dvd(3, "Matrix", 1999, new List<string> { "Action", "Sci-Fi" }, "2L", new Autore("Lana", "Wachowski"), 120));
+            BibliotecaDiBologna.AggiungiDocumento(new Dvd(4, "Inception", 2010, new List<string> { "Action", "Adventure", "Sci-Fi" }, "1A", new Autore("Christopher", "Nolan"), 120));
+
+            BibliotecaDiBologna.AggiungiDocumento(new Libro(5, "Il Signore degli Anelli", 1954, new List<string> { "Fantasy" }, "5M", new Autore("J.R.R.", "Tolkien"), 1178));
+            BibliotecaDiBologna.AggiungiDocumento(new Libro(6, "Harry Potter e la Pietra Filosofale", 1997, new List<string> { "Fantasy" }, "2V", new Autore("J.K.", "Rowling"), 332));
+            BibliotecaDiBologna.AggiungiDocumento(new Libro(7, "Cronache del Ghiaccio e del Fuoco", 1996, new List<string> { "Fantasy" }, "4J", new Autore("George R.R.", "Martin"), 694));
+            BibliotecaDiBologna.AggiungiDocumento(new Libro(8, "1984", 1949, new List<string> { "Dystopian", "Political Fiction" }, "8D", new Autore("George", "Orwell"), 328));
+            BibliotecaDiBologna.AggiungiDocumento(new Libro(9, "Il nome della rosa", 1980, new List<string> { "Historical", "Mystery" }, "7C", new Autore("Umberto", "Eco"), 502));
+            Console.WriteLine("Documenti:");
+            BibliotecaDiBologna.StampaLista(BibliotecaDiBologna.Documenti);
+
+            BibliotecaDiBologna.NuovoPrestito(BibliotecaDiBologna.Utenti[0], BibliotecaDiBologna.Documenti[4], DateTime.Now, BibliotecaDiBologna.DataDiSCadenza(DateTime.Now));
+            BibliotecaDiBologna.NuovoPrestito(BibliotecaDiBologna.Utenti[1], BibliotecaDiBologna.Documenti[3], DateTime.Now, BibliotecaDiBologna.DataDiSCadenza(DateTime.Now));
+            BibliotecaDiBologna.NuovoPrestito(BibliotecaDiBologna.Utenti[2], BibliotecaDiBologna.Documenti[7], DateTime.Now, BibliotecaDiBologna.DataDiSCadenza(DateTime.Now));
+            Console.WriteLine("Prestiti:");
+            BibliotecaDiBologna.StampaLista(BibliotecaDiBologna.Prestiti);
+
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+            BibliotecaDiBologna.StampaLista(BibliotecaDiBologna.CercaDocumentiPerCodice(0));
+            BibliotecaDiBologna.StampaLista(BibliotecaDiBologna.CercaDocumentiPerTitolo("Il Padrino"));
+            BibliotecaDiBologna.StampaLista(BibliotecaDiBologna.CercaPrestitiPerUtente("Gilli", "Max"));
         }
     }
 }
+
---
+++ csharp-biblioteca/Utenti.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Biblioteca
+    {
+        public List<Documento> Documenti { get; set; }
+        public List<Utente> Utenti { get; set; }
+        public List<Prestito> Prestiti { get; set; }
+
+        public Biblioteca()
+        {
+            Documenti = new List<Documento>();
+            Utenti = new List<Utente>();
+            Prestiti = new List<Prestito>();
+        }
+        public List<Documento> CercaDocumentiPerCodice(int codice)
+        {
+            List<Documento> list = new List<Documento>();
+            foreach (Documento documento in Documenti)
+            {
+                if (documento.Id == codice)
+                {
+                    list.Add(documento);
+                }
+            }
+            return list;
+        }
+
+        public List<Documento> CercaDocumentiPerTitolo(string titolo)
+        {
+            List<Documento> list = new List<Documento>();
+            foreach (Documento documento in Documenti)
+            {
+                if (documento.Titolo.Equals(titolo))
+                {
+                    list.Add(documento);
+                }
+            }
+            return list;
+        }
+        public List<Prestito> CercaPrestitiPerUtente(string cognome, string nome)
+        {
+            List<Prestito> list = new List<Prestito>();
+            foreach (Prestito prestito in Prestiti)
+            {
+                if (prestito.Utente.Cognome.Equals(cognome) && prestito.Utente.Nome.Equals(nome))
+                {
+                    list.Add(prestito);
+                }
+            }
+            return list;
+        }
+        public void AggiungiDocumento(Documento documento)
+        {
+            Documenti.Add(documento);
+        }
+
+        public void AggiungiUtente(Utente utente)
+        {
+            Utenti.Add(utente);
+        }
+
+        public void NuovoPrestito(Utente utente, Documento documento, DateTime dataInizio, DateTime dataFine)
+        {
+            Prestito prestito = new Prestito
+            {
+                Utente = utente,
+                Documento = documento,
+                DataInizio = dataInizio,
+                DataFine = dataFine
+            };
+
+            Prestiti.Add(prestito);
+        }
+        public void StampaLista<T>(List<T> lista)
+        {
+            foreach (var elemento in lista)
+            {
+                Console.WriteLine($"- {elemento}");
+            }
+        }
+        public DateTime DataDiSCadenza(DateTime dataInizio)
+        {
+            return dataInizio.AddDays(31);
+        }
+    }
+    public class Utente
+    {
+        public string Cognome { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Telefono { get; set; }
+        public override string ToString()
+        {
+            return $"{Nome} {Cognome}";
+        }
+
+        public Utente(string cognome, string nome, string email, string password, int telefono)
+            {
+                this.Cognome = cognome;
+                this.Nome = nome;
+                this.Email = email;
+                this.Password = password;
+                this.Telefono = telefono;
+            }
+    }
+
+    public class Documento
+    {
+        public int Id { get; set; }
+        public string Titolo { get; set; }
+        public int Anno { get; set; }
+        public List<string> Argomenti { get; set; }
+        public string Scaffale { get; set; }
+        public Autore Autore { get; set; }
+        public override string ToString()
+        {
+            return $"{Titolo} dell'anno {Anno} di {Autore} situato nello scaffale {Scaffale}";
+        }
+
+        public Documento(int id, string titolo, int anno, List<string> argomenti, string scaffale, Autore autore)
+        {
+            this.Id = id;
+            this.Titolo = titolo;
+            this.Anno = anno;
+            this.Argomenti = argomenti;
+            this.Scaffale = scaffale;
+            this.Autore = autore;
+        }
+
+    }
+    public class Autore
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public override string ToString()
+        {
+            return $"{Nome} {Cognome}";
+        }
+        public Autore(string nome, string cognome)
+        {
+            this.Nome = nome;
+            this.Cognome = cognome;
+        }
+    }
+    public class Libro : Documento
+    {
+        public int NumeroPagine { get; set; }
+        public Libro(int id, string titolo, int anno, List<string> argomento, string scaffale, Autore autore, int numeroPagine)
+        : base(id, titolo, anno, argomento, scaffale, autore)
+        {
+            NumeroPagine = numeroPagine;
+        }
+    }
+
+    public class Dvd : Documento
+    {
+        public int Durata { get; set; }
+        public Dvd(int id, string titolo, int anno, List<string> argomento, string scaffale, Autore autore, int durata)
+        : base(id, titolo, anno, argomento, scaffale, autore)
+        {
+            Durata = durata;
+        }
+    }
+    public class Prestito
+    {
+        public Utente Utente { get; set; }
+        public Documento Documento { get; set; }
+        public DateTime DataInizio { get; set; }
+        public DateTime DataFine { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Utente} ha preso in prestito il {Documento} nel {DataInizio} fino al {DataFine}";
+        }
+    }
+    
+}
